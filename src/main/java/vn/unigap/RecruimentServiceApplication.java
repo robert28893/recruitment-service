7dd3bed957bf6fee646e91d8998b76a6c248ce6a
robert <hoangphuongdtvt288@gmail.com>
2023-10-17 21:32:58 +0700

    Update RecruimentServiceApplication.java

--- src/main/java/vn/unigap/RecruimentServiceApplication.java
+++ src/main/java/vn/unigap/RecruimentServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecruimentServiceApplication {
 
+	// chao anh huy
 	public static void main(String[] args) {
 		SpringApplication.run(RecruimentServiceApplication.class, args);
 	}
